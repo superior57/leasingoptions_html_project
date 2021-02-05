06835eebd8d1604903cf73249b621a8283f356ec
talentedexpert0057 <talentedexpert0057@gmail.com>
2021-02-06 03:02:31 +0800

    completed nav bar

--- js/default/active.js
+++ js/default/active.js
@@ -5,21 +5,59 @@
     app.on('load', function () {
         $('#preloader').fadeOut('1000', function () {});
     });
-    // :: Classy Nav Active Code
+    // Classy Nav Active Code
     if ($.fn.classyNav) {
         $('#appNav').classyNav();
     }
 
-    $(document).on('click', '.breakpoint-off .classynav.click-dropdown  ul li', function(e) {
-        let el_li = $(e.target).closest('li');
-        $('.breakpoint-off .classynav.click-dropdown  ul li').removeClass('active');
-        el_li.addClass('active');
-        $('.bg-modal').removeClass('hide');
+    /**
+     * when click background of modal
+     */
+    $('.bg-modal').click(function(e) {
+        $('li').removeClass('active');
+        $(e.target).addClass('hide');        
+        console.log('clicked background of modal');
     });
-    $(document).on('click', '.bg-modal', function(e) {
-        let el = $(e.target);
-        el.addClass('hide');
-        $('.breakpoint-off .classynav.click-dropdown  ul li').removeClass('active');
+    $('.btn-submenu-close').click(function(){
+        $('li').removeClass('active');
+        $('.bg-modal').addClass('hide');
+    });
+    $('.dropdown-submenu').click(function(e) {
+        if($(e.target).hasClass('dropdown-submenu') || $(e.target).hasClass('submenu-pan')) {
+            $('li').removeClass('active');
+            $('.bg-modal').addClass('hide');
+            console.log('clicked submenu');
+        }        
     });
 
+    /**
+     * when click bottom of nav bar
+     */
+    $('.click-dropdown>ul>li').click(function(e) {         
+        if ($(e.target).closest('li').hasClass('cn-dropdown-item')) {
+            $('.click-dropdown>ul>li').removeClass('active');
+            $(e.target).closest('li').addClass('active');
+            $('.bg-modal').removeClass('hide');
+            console.log('clicked nav li');
+        } else {            
+            $('.click-dropdown>ul>li li').removeClass('active');
+            let navbar_li_text = $(">a>span", $(e.target).closest('li').parent().parent()).text();
+            let m_value = 270;
+            console.log($('.dropdown-submenu', $(e.target).closest('li'))[0]);
+            switch(navbar_li_text) {
+                case "Personal Car Leasing" : break;
+                case "Business Car Leasing" : 
+                    m_value = 458; break;
+                case "Van Leasing" : 
+                    m_value = 649; break;
+                case "Special Offers" :
+                    m_value = 776; break;
+                default:
+                    m_value = 270;                    
+            }
+            $('.dropdown-submenu', $(e.target).closest('li')).css('margin-left', `${m_value}px`);
+            $(e.target).closest('li').addClass('active');        
+            console.log('clicked nav- dropdown li');
+        }        
+    });
 })(jQuery);
